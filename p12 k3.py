bd0b325075689687388545e3e55e1986b817c4d5
L200180167 <44256063+L200180167@users.noreply.github.com>
2018-12-23 08:28:09 +0700

    Add files via upload

---
+++ p12 k3.py
@@ -0,0 +1,46 @@
+def hitung(r,phi) :
+    r=Jari Jari
+    phi=phi
+    L=2*(2*phi*r*r)
+    return L
+r = 10
+phi = 22.7
+
+print "<!DOCTYPE html>"
+print
+print """<html>
+<head><title>Luas Bangun Geometri</title></head>
+<body>"""
+print """<table>
+<tr>
+    <th rowspan='8' width='150'> GAMBAR </th>
+    <td><h3> Bangun Geometri <h3></td>
+</tr>
+<tr>
+    <td>Nama Bangun</td>
+    <td>:</td>
+    <td>Bola</td>
+</tr>
+<tr>
+    <td>Dimensi (2D/3D)</td>
+    <td>:</td>
+    <td>3D</td>
+</tr>
+<tr>
+    <td>Rumus Luas</td>
+    <td>:</td>
+    <td>2*(2*phi*r*r)</td>
+</tr>
+<tr>
+    <td>Jari Jari</td>
+    <td>:</td>
+    <td>{}</td>
+</tr>
+<tr>
+    <td>phi</td>
+    <td>:</td>
+    <td>{}</td>
+</tr>
+</table>"""
+
+print"</body></html>"
